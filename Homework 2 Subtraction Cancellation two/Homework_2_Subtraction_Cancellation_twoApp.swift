211711389b982b532825fef3ba15b000c7750718
Matthew Malaker <MMalaker@hawk.iit.edu>
2022-01-28 14:28:25 -0600

    Initial Commit

---
+++ Homework 2 Subtraction Cancellation two/Homework_2_Subtraction_Cancellation_twoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_2_Subtraction_Cancellation_twoApp.swift
+//  Homework 2 Subtraction Cancellation two
+//
+//  Created by Matthew Malaker on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework_2_Subtraction_Cancellation_twoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
